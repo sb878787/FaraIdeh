8fbaecede45be4da24d47bf337949f2c3276fc62
sb878787 <m.sb87.developer@gmail.com>
2025-11-11 18:09:54 +0330

    feat(dashboard): implement header component with profile and icons

---
+++ src/component/icons/dashboard/SearchIcon.tsx
@@ -0,0 +1,26 @@
+'use client';
+
+const SearchIcon = () => {
+  return (
+    <svg width="32" height="32" viewBox="0 0 36 36" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <circle
+        cx="17.6489"
+        cy="17.6499"
+        r="13.4828"
+        stroke="#BBBBBB"
+        strokeWidth="2.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M27.0273 27.7277L32.3134 33"
+        stroke="#BBBBBB"
+        strokeWidth="2.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  );
+};
+
+export default SearchIcon;
--- src/views/dashboard/admin/dashboard/_dashboardpage.tsx
+++ src/views/dashboard/admin/dashboard/_dashboardpage.tsx
@@ -2,28 +2,12 @@
 
 // Components
 import Sidebar from '@/component/dashboard/Sidebar';
+import Header from '@/component/dashboard/Header';
 
 const DashboardPageWrapper = () => {
   return (
     <div className="w-full h-screen grid grid-cols-12">
-      <div className="col-span-10 bg-[#F6F7FB] rtl">
-        <div className="w-full border-b-1 border-[#CCCCCC] bg-white py-5 px-12 flex items-center justify-between">
-          <p className="font-iranYekan text-2xl font-semibold">داشبورد</p>
-
-          <div>
-            <div className="flex items-center gap-4">
-              <div className="w-9 h-9 bg-black rounded-full border border-primary"></div>
-              <div>
-                <p className="text-[#767676] font-iranYekan text-xs">خوش آمدید</p>
-                <p className="text-[#4C4C4C] font-iranYekan line-clamp-1 font-medium">
-                  سید محمد علی صابری
-                </p>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-
+      <Header />
       <Sidebar />
     </div>
   );
