c5477de3a5c6027b85c231ef51caa97d73bc8f60
sb878787 <m.sb87.developer@gmail.com>
2025-11-17 14:29:46 +0330

    feat(dashboard): add Newsletter in dashboard

--- src/views/dashboard/admin/dashboard/MainDashboard.tsx
+++ src/views/dashboard/admin/dashboard/MainDashboard.tsx
@@ -11,6 +11,7 @@ import BlogsIcon from '@/component/icons/dashboard/BlogsIcon';
 import OrdersIcon from '@/component/icons/dashboard/OrdersIcon';
 import FeedbackIcon from '@/component/icons/dashboard/FeedbackIcon';
 import CircleDashboardSVG from '@/component/icons/SVG/CircleDashboardSVG';
+import OurStoryHome from '@/component/icons/SVG/OurStoryHome';
 
 const MainDashboard = () => {
   const dashboardData = [
@@ -47,35 +48,76 @@ const MainDashboard = () => {
   ];
 
   return (
-    <div className="grid grid-cols-3 gap-x-20 gap-y-10 px-12 mt-12 relative">
-      {dashboardData.map((item, index) => (
-        <div key={index} className="w-full bg-white border border-[#D7D7D7] rounded-xl z-40">
-          <div className="py-8 px-8 flex items-center justify-between">
-            <div>
-              {item.icon}
-              <p className="font-iranYekan font-semibold text-xl pt-3">{item.title}</p>
-            </div>
-
-            <div className="relative">
-              <div className="absolute -top-5 -right-9">
-                <ThreeWingsSVG />
+    <>
+      <div className="grid grid-cols-3 gap-x-20 gap-y-10 px-12 mt-12 relative">
+        {dashboardData.map((item, index) => (
+          <div key={index} className="w-full bg-white border border-[#D7D7D7] rounded-xl z-40">
+            <div className="py-8 px-8 flex items-center justify-between">
+              <div>
+                {item.icon}
+                <p className="font-iranYekan font-semibold text-xl pt-3">{item.title}</p>
               </div>
-              <p className="font-yekanBakhFaNum font-semibold text-7xl pt-2">{item.count}</p>
+
+              <div className="relative">
+                <div className="absolute -top-5 -right-9">
+                  <ThreeWingsSVG />
+                </div>
+                <p className="font-yekanBakhFaNum font-semibold text-7xl pt-2">{item.count}</p>
+              </div>
+            </div>
+
+            <div className="border-t border-[#D7D7D7] py-4 pl-8 text-left">
+              <Link href={item.link} className="font-iranYekan text-[#0259FA] hover:underline">
+                مشاهده بیشتر
+              </Link>
             </div>
           </div>
+        ))}
 
-          <div className="border-t border-[#D7D7D7] py-4 pl-8 text-left">
-            <Link href={item.link} className="font-iranYekan text-[#0259FA] hover:underline">
-              مشاهده بیشتر
-            </Link>
-          </div>
+        <div className="absolute top-14 left-0">
+          <CircleDashboardSVG />
         </div>
-      ))}
-
-      <div className="absolute top-14 left-0">
-        <CircleDashboardSVG />
       </div>
-    </div>
+
+      <div className="px-12 mt-16 relative">
+        <div className="absolute -top-5 right-6">
+          <OurStoryHome width="124" height="104" />
+        </div>
+
+        <p className="font-iranYekan font-semibold text-2xl pr-27">خبرنامه</p>
+
+        <div className="w-full bg-white rounded-xl border border-[#D7D7D7] h-36 mt-3 overflow-y-scroll text-left px-5">
+          <p className="font-semibold border-b border-[#D7D7D7] pb-4 mt-3">
+            example@gmail.com{' '}
+            <span className="font-yekanBakhFaNum text-xs text-gray-400 ml-3">1404/5/3</span>
+          </p>
+          <p className="font-semibold border-b border-[#D7D7D7] pb-4 mt-3">
+            example@gmail.com{' '}
+            <span className="font-yekanBakhFaNum text-xs text-gray-400 ml-3">1404/5/3</span>
+          </p>
+          <p className="font-semibold border-b border-[#D7D7D7] pb-4 mt-3">
+            example@gmail.com{' '}
+            <span className="font-yekanBakhFaNum text-xs text-gray-400 ml-3">1404/5/3</span>
+          </p>
+          <p className="font-semibold border-b border-[#D7D7D7] pb-4 mt-3">
+            example@gmail.com{' '}
+            <span className="font-yekanBakhFaNum text-xs text-gray-400 ml-3">1404/5/3</span>
+          </p>
+          <p className="font-semibold border-b border-[#D7D7D7] pb-4 mt-3">
+            example@gmail.com{' '}
+            <span className="font-yekanBakhFaNum text-xs text-gray-400 ml-3">1404/5/3</span>
+          </p>
+          <p className="font-semibold border-b border-[#D7D7D7] pb-4 mt-3">
+            example@gmail.com{' '}
+            <span className="font-yekanBakhFaNum text-xs text-gray-400 ml-3">1404/5/3</span>
+          </p>
+          <p className="font-semibold border-b border-[#D7D7D7] pb-4 mt-3">
+            example@gmail.com{' '}
+            <span className="font-yekanBakhFaNum text-xs text-gray-400 ml-3">1404/5/3</span>
+          </p>
+        </div>
+      </div>
+    </>
   );
 };
 
